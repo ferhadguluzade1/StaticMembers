e940695de883fffbfdbee3251ca37bc3a632e7ed
ferhadguluzade1 <farhadfg-bpa202@code.edu.az>
2025-10-28 10:32:10 +0400

    GamePlayer.cs

---
+++ GamePlayer.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticMembers
+{
+    internal class GamePlayer
+    {
+        public string PlayerName;
+        public int Score { get; set; }
+        public int PlayerID = 1;
+        public static int TotalPlayers { get; set; }
+        public static int HighestScore { get; set; }
+        public static int NextID = 1;
+
+
+        public GamePlayer(int totalPlayers, int playerId, int nextId, int highestScore)
+        {
+            TotalPlayers = totalPlayers;
+            PlayerID = playerId;
+            NextID = nextId;
+            HighestScore = highestScore;
+
+            GamePlayer gamePlayer = new GamePlayer(12, 1, 2, 20);
+            TotalPlayers++;
+
+
+
+        }
+
+
+        public static string playerName(string Name, int NextId):base()
+        {
+            NextID = NextId;
+            NextID++;
+
+
+
+        }
+        public static int PlayerId(int NextId)
+        {
+            //NextID++;
+        }
+        public void UpdateScore(int newScore)
+        {
+            //Score = newScore;
+
+            if (HighestScore <= newScore)
+            {
+                HighestScore = newScore;
+            }
+        }
+        public static void RemovePlayer(GamePlayer p)
+        {
+            //TotalPlayers -= TotalPlayers - p;
+
+            //HighestScore=
+
+
+        }
+
+    }
+}
