5603ca9a4a33ab02a0debf94a51b606e76783034
ferhadguluzade1 <farhadfg-bpa202@code.edu.az>
2025-10-29 21:42:19 +0400

    Program.cs

--- Program.cs
+++ Program.cs
@@ -3,48 +3,48 @@ using System.Numerics;
 
 
 
-Console.WriteLine(
-    "1. \"Add Player: \" \n" +
-    "2. \"Change Scores: \" \n" +
-    "3. \"Remove Player ( input id: \")\n" +
-    "4. \"All Player info: \" \n" +
-    "5. \"Player Name: \"\n" +
-    "6. \"Player Id: \"\n" +
-    "7. \"Total Players: \"\n" +
-    "8. \"The Highest Score: \""
-    );
-Console.WriteLine("Choose proses: ");
-int input = Convert.ToInt32(Console.ReadLine());
-//if (input <= 8)
-//{
-switch (input)
-    {
-        case 1:
-        Console.WriteLine();
-        break;
-        case 2:
-        Console.WriteLine();
-        break;
-        case 3:
-        Console.WriteLine();
-        break;
-        case 4:
-        Console.WriteLine();
-        break;
-        case 5:
-        Console.WriteLine();
-        break;
-        case 6:
-        Console.WriteLine();
-        break;
-        case 7:
-        Console.WriteLine();
-        break;
-        case 8:
-        Console.WriteLine();
-        break;
-        default:
 
-        break;
+while (true)
+{
+    Console.WriteLine(
+        "1. \"Add Player: \" \n" +
+        "2. \"Change Scores: \" \n" +
+        "3. \"Remove Player ( input id: \")\n" +
+        "4. \"All Player info: \" \n" +
+        "5. Exit Program"
+        );
+    Console.WriteLine("Choose proses: ");
+    string input = Console.ReadLine();
+    {
+        switch (input)
+        {
+            case "1":
+
+                Console.WriteLine();
+                break;
+
+            case "2":
+
+                Console.WriteLine();
+                break;
+
+            case "3":
+                Console.WriteLine(GamePlayer.RemovePlayer);
+                break;
+
+            case "4":
+
+                Console.WriteLine(GamePlayer.players);
+                break;
+
+            case "5":
+                break;
+
+            default:
+                Console.WriteLine("Wrong input");
+                break;
+
+        }
 
     }
+}
